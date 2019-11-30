3b06c8184588f60a586dad007776c6e643f40406
Moussa SARR <einsarr@gmail.com>
2019-12-01 00:41:41 +0100

    Ajout image isi sur index

--- index.php
+++ index.php
@@ -3,4 +3,11 @@
 
 <?php require_once "public/template/menu.php";?>
 
+
+<div class="">
+    <img src="public/images/isi.png" alt="">
+    <h1>GESTION DES COMPTES</h1>
+</div>
+
+
 <?php require_once "public/template/footer.php";?>
